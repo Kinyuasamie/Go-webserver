705339b4404dc1f303e6173bb515a0766ed3ebd7
unknown <samiekinyua322@gmail.com>
2025-08-08 20:19:06 +0300

    Added new feature

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name = "samuel"
+	surname := "kinyua"
+	fmt.Print("hello", name, "my other name is", surname)
+	fmt.Print("are you still there baddy keep nailing that axle its very important amnh")
+
+}
