488ea8819d7724248fa4ba182ced268414046332
unknown <samiekinyua322@gmail.com>
2025-08-10 19:06:46 +0300

    more featurs

--- main.go
+++ main.go
@@ -1,11 +1,16 @@
 package main
 
-import "fmt"
+import ("fmt"
+  "log"
+"net/http")
 
 func main() {
-	var name = "samuel"
-	surname := "kinyua"
-	fmt.Print("hello", name, "my other name is", surname)
-	fmt.Print("are you still there baddy keep nailing that axle its very important amnh")
-
+	fileserver := http.FileServer(http.Dir("./static"))
+http.Handle("/",fileserver)
+http.HandleFunc("/form",formHandler)
+http.HandleFunc("/hello",helloHandler)
+fmt.Printf("starting server at port 8080\n")
+if err := http.ListenAndServe(":8080",nil), err != nil {
+	log.Fatal(err)
+}
 }
